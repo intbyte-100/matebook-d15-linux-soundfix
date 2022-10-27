058769f33aec779c6939472f40a6de104a3baedc
intbyte-100 <5656ltl@gmail.com>
2022-10-27 14:52:55 +0300

    fix installer

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ printf "[ Compiling... ]\n\n"
 gcc -O3 -Os -Wall main.c -o mbsoundfix -lm $(pkg-config --cflags --libs libpipewire-0.3)
 printf "[ Copying files... ]\n"
 cp ./mbsoundfix /usr/bin
-cp ./mbsoundfix.service /etc/systemd/system/
+cp ./mbsoundfix.service /etc/systemd/user/
 systemctl daemon-reload
 systemctl status mbsoundfix.service 
 printf "\n[ mbsoundfix service was installed, use it with systemctl ]\n"
